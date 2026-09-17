ae4070f0a9a26e8c1b29e819fc02b74173f1e7ae
IsaacMorillo <irmorillowork@gmail.com>
2026-09-16 23:15:41 -0500

    Create modular functions for basic arithmetic operations
    Implement individual functions for addition, subtraction, multiplication,
    division, and percentage to handle calculation logic cleanly.

--- main.js
+++ main.js
@@ -27,3 +27,23 @@ operatorButtons.forEach((button) =>{
     })
 })
 
+function add (num1, num2){
+    return num1 + num2;
+}
+
+function substract (num1, num2){
+    return num1 - num2;
+}
+
+function multiply (num1, num2){
+    return num1 * num2;
+}
+
+function divide (num1, num2){
+    return num1 / num2;
+}
+
+function percentage (num1, num2){
+    num1 = num1/100;
+    return num1 * num2;
+}
